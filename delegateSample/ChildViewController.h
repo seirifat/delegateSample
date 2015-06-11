1758b7b2e34649f0897f69091a63610b331613bc
Rifat Firdaus <rifat@Suitmedias-Mac-mini.local>
2015-06-11 10:09:26 +0700

    Delegate Prepare --sei

---
+++ delegateSample/ChildViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChildViewController.h
+//  delegateSample
+//
+//  Created by Rifat Firdaus on 6/11/15.
+//  Copyright (c) 2015 Rifat Firdaus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChildViewController : UIViewController
+
+@end
---
+++ delegateSample/ParentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ParentViewController.h
+//  delegateSample
+//
+//  Created by Rifat Firdaus on 6/11/15.
+//  Copyright (c) 2015 Rifat Firdaus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParentViewController : UIViewController
+
+@end
